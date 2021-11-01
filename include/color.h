fd0afe86ab63fc637b069b796d79385ed67262e5
vandark <vandark@hust.edu.cn>
2021-11-01 20:06:56 +0800

    color support

---
+++ include/color.h
@@ -0,0 +1,31 @@
+/*
+ * Filename: color.h
+ * Auther: Zhang Wenda <vaaandark@foxmail.com>
+ * Creation time: 2021-10-31 21:16:23
+ * Latest change: 2021-10-31 21:16:23
+ * License: GPL-2.0
+ * Description: Color support 
+ */
+/* reset */
+#define COLOR_NONE "\033[0m"
+
+/* Regular Colors */
+#define COLOR_BLACK     "\033[0;30m"
+#define COLOR_RED       "\033[0;31m"
+#define COLOR_GREEN     "\033[0;32m"
+#define COLOR_YELLOW    "\033[0;33m"
+#define COLOR_BLUE      "\033[0;34m"
+#define COLOR_PURPLE    "\033[0;35m"
+#define COLOR_CYAN      "\033[0;36m"
+#define COLOR_WHITE     "\033[0;37m"
+
+/* Bold */
+#define COLOR_BBLACK    "\033[1;30m"
+#define COLOR_BRED      "\033[1;31m"
+#define COLOR_BGREEN    "\033[1;32m"
+#define COLOR_BYELLOW   "\033[1;33m"
+#define COLOR_BBLUE     "\033[1;34m"
+#define COLOR_BPURPLE   "\033[1;35m"
+#define COLOR_BCYAN     "\033[1;36m"
+#define COLOR_BWHITE	"\033[1;37m"
+
